1e43e0225b8410605fd391dff6f902257ee9c291
zhanglirong1999 <213182194@seu.edu.cn>
2020-10-11 15:49:08 +0800

    cors

--- src/main/java/com/school/uniform/common/WebMvcConfig.java
+++ src/main/java/com/school/uniform/common/WebMvcConfig.java
@@ -1,4 +1,18 @@
 package com.school.uniform.common;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    //解决跨域问题
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
 }
